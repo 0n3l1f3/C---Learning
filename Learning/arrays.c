b509c44263a81fc0027f4f4aae96dfe76a086721
0n3l1f3 <eghosaendurrance@gmail.com>
2026-06-02 21:09:38 +0100

    Arrays

---
+++ Learning/arrays.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    /*
+    int i; 
+    float f[4]; 
+
+    f[0] = 3.14159;  // Indexing starts at 0, of course.
+    f[1] = 1.41421;
+    f[2] = 1.61803;
+    f[3] = 2.71828; 
+    for (i = 0 ; i< 4; i++)
+    {
+        printf("%f\n",f[i]);
+    }
+        */
+       
+}
